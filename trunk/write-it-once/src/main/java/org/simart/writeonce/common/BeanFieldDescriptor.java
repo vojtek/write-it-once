d62e262f32decaca40d409284fe42f28b14e4986
Wojciech Kołodziej <vojtekkol@gmail.com>
2014-06-12 19:51:57 +0200

    project reconfiguration

--- trunk/write-it-once/src/main/java/org/simart/writeonce/common/BeanFieldDescriptor.java
+++
@@ -1,8 +0,0 @@
-package org.simart.writeonce.common;
-
-public interface BeanFieldDescriptor extends FieldDescriptor {
-
-    BeanMethodDescriptor getGetter();
-
-    BeanMethodDescriptor getSetter();
-}
--- trunk/write-it-once/src/main/java/org/simart/writeonce/common/ColumnNameResolver.java
+++
@@ -1,9 +0,0 @@
-package org.simart.writeonce.common;
-
-
-public interface ColumnNameResolver {
-
-    String getName(MethodDescriptor method);
-
-    String getName(FieldDescriptor field);
-}
